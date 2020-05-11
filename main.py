1053044e95e253ba77f3a2576eb085a919608cdb
vindaloo95 <vindaloo95@yahoo.com>
2020-05-11 01:53:35 +0000

    Wrote some basic python commands

---
+++ main.py
@@ -0,0 +1,20 @@
+
+# This is a comment
+# a;skldjfklajsdf
+# alsjaklsdjf
+# hello this is a test
+
+print('''Hello World
+sjdflkj
+
+lasjdklfjds
+
+kasd;fjasdf
+
+
+
+This is a new line''')
+print (20 * 56)
+print("45 * 45 = " + '54')
+print('67 + 876 = ' + str(67 + 876))
+print('I am in 6th grade')
